4e800e64000f4528c067e36636f4aae9bed54e4c
TLuco <toni.lucinger@skole.hr>
2022-12-07 18:00:40 +0100

    prekjucer

--- app/src/main/java/com/example/luco/MainActivity.java
+++ app/src/main/java/com/example/luco/MainActivity.java
@@ -45,11 +45,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+
+    zrtzhtrztrzrt
+
+        re    @Override
+        public boolean onCreateOptionsMen7u(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+       
     }
 
     @Override
